8f8f9024272c39716909da715f736d8a9da819e4
arsalan.ahmed <arsalanabc@gmail.com>
2024-10-15 08:55:15 -0400

    Use redis cache

--- src/main/java/com/ninjaone/dundie_awards/DundieAwardsApplication.java
+++ src/main/java/com/ninjaone/dundie_awards/DundieAwardsApplication.java
@@ -3,7 +3,13 @@ package com.ninjaone.dundie_awards;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
 
 @SpringBootApplication
 @EnableCaching
